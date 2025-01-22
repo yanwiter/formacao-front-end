a2c6d569776f0074d56ab1de26e50b347474f4e4
Yan Witer Rocha Barbosa <yan.barbosa@ketra.com.br>
2025-01-22 15:33:49 -0300

    Terceiro desafio da formação

---
+++ React/Desafios e testes de conhecimento/desafio-3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Terceiro desafio da formação 🚀</h1>
+        <p>Altere o titulo da página</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
