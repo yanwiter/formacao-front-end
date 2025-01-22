6ccfd9bb374444716fd810e4c761150f63e6e43c
Yan Witer Rocha Barbosa <yan.barbosa@ketra.com.br>
2025-01-22 11:56:46 -0300

    Primeiro desafio da formação

---
+++ React/Desafios e testes de conhecimento/desafio-1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Primeiro desafio da formação</h1>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
